2b28ba25d0ce943495a93491d4660f471ccdc154
thementors <thementors.bootcamps@hotmail.com>
2022-03-19 23:29:13 -0600

    ....

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+git config --global user.email "christian.ibarra.thementors@gmail.com"
+git config --global user.name "Christian Ibarra"
+
+wget https://api.render.com/deploy/srv-c8r5ehs6fj31qhot72tg?key=iDgyiH1utSA
--- shell.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.email "christian.ibarra.thementors@gmail.com"
-git config --global user.name "Christian Ibarra"
