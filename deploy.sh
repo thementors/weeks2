af089dc2dfacd3e0dc3836d3a5c128130d41c037
thementors <thementors.bootcamps@hotmail.com>
2022-03-21 18:25:51 -0600

    Updating

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-git config --global user.email "christian.ibarra.thementors@gmail.com"
-git config --global user.name "Christian Ibarra"
-
-wget https://api.render.com/deploy/srv-c8r5ehs6fj31qhot72tg?key=iDgyiH1utSA
+git add .
+git commit -m "Updating"
+git push origin/main
